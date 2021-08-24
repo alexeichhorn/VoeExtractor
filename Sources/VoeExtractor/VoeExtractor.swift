0023f293de4b32beb9dfbc60b315833771d07d1f
Alexander Eichhorn <a.eichhorn@losjet.com>
2021-08-24 17:54:13 +0200

    added async wrapper function for new iOS 15, macOS 12, ... release cycle

--- Sources/VoeExtractor/VoeExtractor.swift
+++ Sources/VoeExtractor/VoeExtractor.swift
@@ -56,4 +56,21 @@ public class VoeExtractor {
         #endif
     }
     
+    
+    #if swift(>=5.5)
+    
+    /// extracts direct video url or streaming manifest from standard voe url
+    /// - parameter url: voe url (e.g.: https://voe.sx/e/8vi96tm5uufc)
+    /// - returns: video url, if found (else: nil)
+    @available(iOS 15.0, macOS 12.0, watchOS 8.0, tvOS 15.0, *)
+    public class func extract(fromURL url: URL) async -> URL? {
+        await withCheckedContinuation { continuation in
+            extract(fromURL: url) { videoURL in
+                continuation.resume(returning: videoURL)
+            }
+        }
+    }
+    
+    #endif
+    
 }
